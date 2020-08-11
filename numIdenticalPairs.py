d7e4e63b90e299981fb36acea341fff259641dac
Fatih Karabacak <fatihtx@gmail.com>
2020-08-11 15:12:04 -0700

    Update numIdenticalPairs.py

--- numIdenticalPairs.py
+++ numIdenticalPairs.py
@@ -1,8 +1,11 @@
 class Solution:
     def numIdenticalPairs(self, nums: List[int]) -> int:
         counter=0
-        for i in range(len(nums)):
-            for k in range(i+1,len(nums)):
-                if nums[i] == nums[k]:
-                    counter +=1
+        nums.sort()
+        i = 0
+        for j in range(1,len(nums)):
+            if nums[i] == nums[j]:
+                counter += j - i 
+            else:
+                i = j
         return counter
