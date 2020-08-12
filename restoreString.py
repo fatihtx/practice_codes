9d525e906669541c3aeadc115da5b1bf1e598311
Fatih Karabacak <fatihtx@gmail.com>
2020-08-12 12:57:46 -0700

    Update restoreString.py

--- restoreString.py
+++ restoreString.py
@@ -4,4 +4,12 @@ class Solution:
         merged_list = [(indices[i], list_s[i]) for i in range(0, len(list_s))]
         merged_list.sort()  # sort tuple by second element -->  merged_list.sort(key=lambda x:x[1])
         out = list(zip(*merged_list))
+        
+        '''
+        res = [''] * len(s)
+        for index, char in enumerate(s):
+            res[indices[index]] = char
+        return "".join(res)
+        '''
+     
         return ''.join(out[1])
