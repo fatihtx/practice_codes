deec8607a8cb49b71f83ab94bf53dc4c3c7831b6
Fatih Karabacak <fatihtx@gmail.com>
2020-08-12 12:55:49 -0700

    Update restoreString.py

--- restoreString.py
+++ restoreString.py
@@ -2,6 +2,6 @@ class Solution:
     def restoreString(self, s: str, indices: List[int]) -> str:
         list_s = list(s) 
         merged_list = [(indices[i], list_s[i]) for i in range(0, len(list_s))]
-        merged_list.sort()
+        merged_list.sort()  # sort tuple by second element -->  merged_list.sort(key=lambda x:x[1])
         out = list(zip(*merged_list))
         return ''.join(out[1])
