2a17b7606bea4ed6d0661de3b66c6864948e8a97
Fatih Karabacak <fatihtx@gmail.com>
2020-08-11 17:48:09 -0700

    Create numJewelsInStones.py

---
+++ numJewelsInStones.py
@@ -0,0 +1,9 @@
+class Solution:
+    def numJewelsInStones(self, J: str, S: str) -> int:
+        count = 0
+        letter_dict = dict()
+        list_of_uniq_char_S = list(set(S))
+        list_of_uniq_char_J = list(set(J))
+        for i in list_of_uniq_char_J:
+            count = count + S.count(i) 
+        return count
