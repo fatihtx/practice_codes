24dd89a0b4bd5aa7295aa24d59ef16a5034b142c
Fatih Karabacak <fatihtx@gmail.com>
2020-08-16 12:08:17 -0700

    Update getFolderNames.py

--- getFolderNames.py
+++ getFolderNames.py
@@ -18,3 +18,19 @@ class Solution:
             else:
                 folder_dict[name] = 1
         return names
+'''
+    from collections import defaultdict
+    def getFolderNames(self, names: List[str]) -> List[str]:
+        used, hashmap = set(), defaultdict(int)
+        result = []
+        for name in names:
+            k = hashmap[name]
+            current = name
+            while current in used:
+                k += 1
+                current = '%s(%d)' % (name, k)  # alternative to current = name+'('+str(k)+')'
+            hashmap[name] = k
+            result.append(current)
+            used.add(current)
+        return result
+'''
