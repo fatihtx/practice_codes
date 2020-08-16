3734435e2635224e4c933d7a9eed1aa043a5a54b
Fatih Karabacak <fatihtx@gmail.com>
2020-08-16 13:23:14 -0700

    Update balancedStringSplit.py

--- balancedStringSplit.py
+++ balancedStringSplit.py
@@ -14,3 +14,13 @@ class Solution:
                 count_r =0
                 count_l =0
         return return_count
+
+    '''
+    def balancedStringSplit(self, s: str) -> int:
+    res = cnt = 0         
+    for c in s:
+        cnt += 1 if c == 'L' else -1            
+        if cnt == 0:
+            res += 1
+    return res  
+    '''
