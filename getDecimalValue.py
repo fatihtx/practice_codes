7b0ee619483109f558c8f76a40d51bc5e0b7e82c
Fatih Karabacak <fatihtx@gmail.com>
2020-08-29 12:39:32 -0700

    Create getDecimalValue.py

---
+++ getDecimalValue.py
@@ -0,0 +1,13 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def getDecimalValue(self, head: ListNode) -> int:
+        number_list = list()
+        curr = head
+        while curr:
+            number_list.append(curr.val)
+            curr = curr.next 
+        return int("".join(str(i) for i in number_list),2)
